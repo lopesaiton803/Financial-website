0433de13ea661c054709beba50c84c92d702a99e
lopesaiton <lopesaiton803@gmail.com>
2025-10-07 11:03:06 +0200

    nova atulizacao 11:03 pm

--- js/main.js
+++ js/main.js
@@ -305,3 +305,17 @@ document.addEventListener('DOMContentLoaded', function() {
         }
     }
 });
+
+
+// email js
+
+function sendMail(){
+    let parms = {
+        name : document.getElementById("name").value,
+        email : document.getElementById("email").value,
+          subject : document.getElementById("subject").value,
+            message : document.getElementById("message").value,
+    }
+
+    emailjs.send("lopesaiton803", "template_8hgfbx6", parms).then(alert("Email Sent!!"))
+}
